9e32e24524ce2a1832ba89d0f94d011952d7d6ab
Aliaksandr Valialkin <valyala@victoriametrics.com>
2025-12-07 14:04:19 +0100

    apptest/tests/lastnoptimization_test.go: verify additional cases when the logs are located in the middle and at the end of the selected time range
    Plus verify cases when logs are missing on the selected time range
    
    Updates https://github.com/VictoriaMetrics/VictoriaLogs/issues/802

--- apptest/tests/lastnoptimization_test.go
+++ apptest/tests/lastnoptimization_test.go
@@ -28,26 +28,66 @@ func TestVlsingleLastnOptimization(t *testing.T) {
 	sut.JSONLineWrite(t, ingestRecords, apptest.IngestOpts{})
 	sut.ForceFlush(t)
 
-	for limit := 1; limit <= 2*len(ingestRecords); limit++ {
-		var logLines []string
+	f := func(start, end string) {
+		t.Helper()
 
-		wantLinesCount := limit
-		if wantLinesCount > len(ingestRecords) {
-			wantLinesCount = len(ingestRecords)
-		}
-		for i := 0; i < wantLinesCount; i++ {
-			logLines = append(logLines, ingestRecords[i])
-		}
-		wantResponse := &apptest.LogsQLQueryResponse{
-			LogLines: logLines,
-		}
+		for limit := 1; limit <= 2*len(ingestRecords); limit++ {
+			var logLines []string
 
-		selectQueryArgs := apptest.QueryOpts{
-			Start: "2025-01-01T01:00:00Z",
-			End:   "2025-01-01T01:00:03Z",
-			Limit: fmt.Sprintf("%d", limit),
+			wantLinesCount := limit
+			if wantLinesCount > len(ingestRecords) {
+				wantLinesCount = len(ingestRecords)
+			}
+			for i := 0; i < wantLinesCount; i++ {
+				logLines = append(logLines, ingestRecords[i])
+			}
+			wantResponse := &apptest.LogsQLQueryResponse{
+				LogLines: logLines,
+			}
+
+			selectQueryArgs := apptest.QueryOpts{
+				Start: start,
+				End:   end,
+				Limit: fmt.Sprintf("%d", limit),
+			}
+			got := sut.LogsQLQuery(t, "* | keep _msg, _time", selectQueryArgs)
+			assertLogsQLResponseEqual(t, got, wantResponse)
 		}
-		got := sut.LogsQLQuery(t, "* | keep _msg, _time", selectQueryArgs)
-		assertLogsQLResponseEqual(t, got, wantResponse)
 	}
+
+	// verify the case when the logs are located at the start of the selected time range
+	f("2025-01-01T01:00:00Z", "2025-01-01T01:00:03Z")
+
+	// verify the case when the logs are located in the middle of the selected time range
+	f("2024-12-31T23:59:59Z", "2025-01-01T01:00:03Z")
+
+	// verify the case when the logs are located at the end of the selected time range
+	f("2024-12-31T23:59:59Z", "2025-01-01T01:00:00.000000001Z")
+
+	// verify the case when the logs are outside the selected time range
+	selectQueryArgs := apptest.QueryOpts{
+		Start: "2024-12-31T23:59:59Z",
+		End: "2025-01-01T01:00:00Z",
+		Limit: "3",
+	}
+	got := sut.LogsQLQuery(t, "* | count() x", selectQueryArgs)
+	wantResponse := &apptest.LogsQLQueryResponse{
+		LogLines: []string{
+			`{"x":"0"}`,
+		},
+	}
+	assertLogsQLResponseEqual(t, got, wantResponse)
+
+	selectQueryArgs = apptest.QueryOpts{
+		Start: "2025-01-01T01:00:00.000000001Z",
+		End: "2025-01-01T01:00:03Z",
+		Limit: "3",
+	}
+	got = sut.LogsQLQuery(t, "* | count() x", selectQueryArgs)
+	wantResponse = &apptest.LogsQLQueryResponse{
+		LogLines: []string{
+			`{"x":"0"}`,
+		},
+	}
+	assertLogsQLResponseEqual(t, got, wantResponse)
 }
